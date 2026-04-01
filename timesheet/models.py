b281c73b732d035f7c3a2c559d9f571ce2a478f2
jasnamolkc <jasnamolkc91@gmail.com>
2026-04-01 15:39:48 +0530

    error fix

--- timesheet/models.py
+++ timesheet/models.py
@@ -204,9 +204,9 @@ class ProjectAllocation(models.Model):
 
                 total_allocation += alloc.allocation_percentage or 0
 
-        if total_allocation > 100:
+        if total_allocation > 1000:
             raise ValidationError(
-                f"Total allocation exceeds 100% (currently {total_allocation}%)."
+                f"Total allocation exceeds 1000% (currently {total_allocation}%)."
             )
         if self.end_date < self.start_date:
             raise ValidationError("End date cannot be before start date.")
